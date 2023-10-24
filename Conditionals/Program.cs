19c30b6cd70d326a43b58161f8bf42e1dc387a43
busraknar <b.ertas91gmail.com>
2023-10-24 21:19:54 +0300

    Conditionals

---
+++ Conditionals/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+            //if(number ==10)
+            //{
+            //    Console.WriteLine("Number is 10");
+            //}
+            //else if(number==20)
+            //{
+            //    Console.WriteLine("Number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is not 10 or 20");
+            //}
+
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+
