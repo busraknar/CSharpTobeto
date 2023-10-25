015c8c50f75acde4e23a83016f764345f8f0ba06
busraknar <b.ertas91gmail.com>
2023-10-26 00:21:56 +0300

    Classes/ Encapsulation

---
+++ Classes/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Customer
+    {
+        //field
+        //  public string FirstName;
+
+        //Property
+        //prop  yazıp tab a bas
+        public int Id { get; set; }   //Müşterilerin özelliklerini  tutmak için kullandığımız nesne
+        public string FirstName { get; set; }
+        //private string _firstName;
+        //public string FirstName
+        //{
+        //    get { return "Mrs."+ _firstName; }
+        //    set { _firstName = value; }
+        //}
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(); // bu classın örneğini oluşturduk
+            customerManager.Add();
+            customerManager.Update();
+
+
+            ProductManager productManager = new ProductManager(); // bu classın örneğini oluşturduk
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Engin";  // eşittir dediğimiz zaman SET bloğu çalışır
+            customer.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                Id=2, City="İstanbul", FirstName="Derin", LastName="Demiroğ"            
+            };
+            Console.WriteLine(customer2.FirstName);  //burada çalışan GET bloğudur.
+            Console.ReadLine();
+        }
+    }
+    //class CustomerManager
+    //{
+    //    public void Add()
+    //    {
+    //        Console.WriteLine("Customer Added!");
+    //    }
+    //    public void Update()
+    //    {
+    //        Console.WriteLine("Customer Updated!");
+    //    }
+    //}
+    //class ProductManager
+    //{
+    //    public void Add()
+    //    {
+    //        Console.WriteLine("Product Added!");
+    //    }
+    //    public void Update()
+    //    {
+    //        Console.WriteLine("Product Updated!");
+    //    }
+    //}
+}
